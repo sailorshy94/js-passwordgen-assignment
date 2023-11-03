0633dae6ffc0b4fbe785c8cd7cc7732bcd77289b
sailorshy94 <sbutle94@gmail.com>
2023-11-02 22:43:37 -0400

    repaired broken link to script.js in html, added comments for psuedocode, added variable specialChar array under function generatePassword

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 // Assignment Code
 // debugger;
-
+var document = 
 // var generateBtn = ; 
 // - when ran node script.js ReferenceError: document is not defined???
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
   // ADD CODE HERE
+  var specialChar = ["!", "#", "$", "%", "&", "'", "()", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~", "]"];
 }
+// password length 8-128 characters
+// character types:
+// lowercase
+// uppercase
+// numeric
+// and/or special characters
 
 // Write password to the #password input
 function writePassword() {

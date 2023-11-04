a3b3887d5c544ff14ea04edd3ace54a8f2b84f72
sailorshy94 <sbutle94@gmail.com>
2023-11-03 22:15:07 -0400

    added loops for all the password characters, tested output in browser console

--- script.js
+++ script.js
@@ -13,24 +13,39 @@ function generatePassword() {
   var lowercase = "abcdefghijklmnopqrstuvwxyz";
   var uppercase = lowercase.toUpperCase();
 
+  for (var i = 0; i < specialChar.length; i++){
+    console.log(specialChar[i]);
+  }
+
+  for (var i = 0; i < number.length; i++){
+    console.log(number[i]);
+  }
+
   for (var i = 0; i < lowercase.length; i++){
     console.log(lowercase[i]);
   }
-  return;
+
+  for (var i = 0; i < uppercase.length; i++){
+    console.log(uppercase[i]);
+  }
+
+  // return;
 }
 
 // password length 8-128 characters
-// if, else statement?? If passwordLength is between 8 to 128 characters then it will generate a password, else it will not generate a password
+// if, else statement?? If passwordLength is between 8 to 128 characters( <= ???? ) then it will generate a password, else it will not generate a password
 // character types:
 // lowercase
 // uppercase
 // numeric
 // and/or special characters
 
-// Write password to the #password input - what does this mean????
+// basically want to tell the js to run when generate pwd button pushed and to select between 8 to 128 characters randomly from the lists of character types
+
+// Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector("#password"); //,password); ????
+  var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 

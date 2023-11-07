84fafdc488d97d5d9e8fbfe9ccabdf53a0559968
sailorshy94 <sbutle94@gmail.com>
2023-11-06 22:28:45 -0500

    added comments for application behavior after adding for loop

--- script.js
+++ script.js
@@ -11,25 +11,25 @@ function writePassword() {
   // var possibleChar = specialChar, lowercaseChar, uppercaseChar, numeralChar;?
   // can I come back and consolidate the variables into something more condensed? 
   var possibleChar = "";
-  var specialChar = " !'#$%^&*@()_-+=`~{|}/[]<>;:.";
+  var specialChar = "!'#$%^&*@()_-+=`~{|}/[]<>;:.";
   var lowercaseChar = "abcdefghijklmnopqrstuvwxyz";
   var uppercaseChar = lowercaseChar.toUpperCase;
   var numeralChar = "123456789";
 
-  var inputLength = 17;
+  var inputLength = 10;
   var hasSpecialChar = false;
   var hasLowercaseChar = false;
   var hasUppercaseChar = false;
   var hasNumeralChar = false;
 
   // test the application
-  for (var i = 0; i < inputLength; i++)
-    var random = Math.floor(Math.random() * specialChar.length)
-    password += specialChar[random]; 
-    
+  // for (var i = 0; i < inputLength; i++)
+  //   var random = Math.floor(Math.random() * specialChar.length)
+  //   password += specialChar[random]; 
+// output of for loop = all the specialChars at one time with [object HTMLTextAreaElement] before it?????
+
   passwordText.value = password;
 
 }
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);

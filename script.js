fed4709ff7976a88a01e1f69fa7bfcac5f75b517
sailorshy94 <sbutle94@gmail.com>
2023-11-07 17:08:33 -0500

    added console.log method for password string and checked in web browser console for proper function

--- script.js
+++ script.js
@@ -4,23 +4,24 @@ var generateBtn = document.querySelector("#generate");
 function generatePassword() {
   // ADD CODE HERE
   // can I come back and consolidate the variables into something more condensed? 
-  var possibleChar = "";
-  var specialChar = "!'#$%^&*@()_-+=`~{|}/[]<>;:.";
-  var lowercaseChar = "abcdefghijklmnopqrstuvwxyz";
-  var uppercaseChar = lowercaseChar.toUpperCase;
-  var numeralChar = "123456789";
+  // var possibleChar = "";
+  // var specialChar = "!'#$%^&*@()_-+=`~{|}/[]<>;:.";
+  // var lowercaseChar = "abcdefghijklmnopqrstuvwxyz";
+  // var uppercaseChar = lowercaseChar.toUpperCase;
+  // var numeralChar = "123456789";
 
-  var inputLength = 10;
-  var hasSpecialChar = false;
-  var hasLowercaseChar = false;
-  var hasUppercaseChar = false;
-  var hasNumeralChar = false;
+  // var inputLength = 10;
+  // var hasSpecialChar = false;
+  // var hasLowercaseChar = false;
+  // var hasUppercaseChar = false;
+  // var hasNumeralChar = false;
 
   // test the application
   // for (var i = 0; i < inputLength; i++)
   //   var random = Math.floor(Math.random() * specialChar.length)
   //   password += specialChar[random]; 
   // output of for loop = all the specialChars at one time with [object HTMLTextAreaElement] before it?????  
+  console.log("password");
   return "password";
 
 }

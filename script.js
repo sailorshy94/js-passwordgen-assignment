785c0990d36425fc60892705442dbd599437984a
sailorshy94 <sbutle94@gmail.com>
2023-11-08 17:11:44 -0500

    added if statements for input length to keep it between 8 and 128 characters

--- script.js
+++ script.js
@@ -15,26 +15,38 @@ function generatePassword() {
   // var inputLength = Math.floor((Math.random() * 128) + 8); - do not need this for acceptance criteria; user must be able to input the length they want
   // will need parameters for too short vs too long
   // need to assign the variables here window.confirm & remove the hard coding of everything below
-  var inputLength = window.prompt("Include how many characters (between 8 to 128)?")
+  var inputLength = window.prompt("Include how many characters (between 8 to 128)?");
   var hasSpecialChar = window.confirm("Include special characters?");
   var hasLowercaseChar = window.confirm("Include lowercase characters?");
   var hasUppercaseChar = window.confirm("Include uppercase letters?");
   var hasNumeralChar = window.confirm("Include numeric characters?");
+  var invalidLength = "Please try again, insufficient user input.";
 
   for (var i = 0; i < inputLength; i++){
     var random = Math.floor(Math.random() * charCombo.length)
     possibleChar += charCombo[random];
   }
 
+  if (inputLength < 8){
+    alert("Please choose a length of at least 8 characters or more.")
+    return invalidLength;
+  }
+
+  if (inputLength > 128){
+    alert("Please choose a length between 8 and 128 characters.");
+    return invalidLength;
+  }
+  
+  
   // need to change the way this is set up
-//   if (hasSpecialChar || hasLowercaseChar || hasUppercaseChar || hasNumeralChar) {
-//     console.log(possibleChar);
-//     return possibleChar;
-//   } else {
-//     alert("User must select at least one character type.");
-//     return;
-//   }
-// }
+  if (hasSpecialChar || hasLowercaseChar || hasUppercaseChar || hasNumeralChar) {
+    console.log(possibleChar);
+    return possibleChar;
+  } else {
+    alert("User must select at least one character type.");
+    return;
+  }
+}
 
 // Write password to the #password input
 function writePassword() {
